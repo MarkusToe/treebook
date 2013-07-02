cfef5812e2117025ad9bb5c5ca5cd7b36ca4ca84
Markus Török <markus.toeroek@gmail.com>
2013-07-02 22:25:46 +0200

    Add Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '3ed05ee2a42e8e8b282982c3577bae3bcaf6a740e5b13fdbf61c23a866b33819fda5c8edb40867c861e14008a8f89a59afe297b41efe48792953f2fb3d17b7ec'
