95ae97928376056b2481a73b891d13313b82f98a
Markus Török <markus.toeroek@gmail.com>
2013-08-18 20:37:49 +0200

    Added tests for custom routes

---
+++ test/integration/custom_routes_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+# Integration tests are used to test the interaction among any number of controllers. 
+# They are generally used to test important work flows within your application.
+
+class CustomRoutesTest < ActionDispatch::IntegrationTest
+	test "that /login route opens the login page" do
+		get 'login'
+		assert_response :success
+	end
+
+	test "that /logout route opens the log out page" do
+		get 'logout'
+		assert_response :redirect
+		assert_redirected_to '/'
+	end
+
+	test "that /register route opens the sing up page" do
+		get 'register'
+		assert_response :success
+	end
+end
